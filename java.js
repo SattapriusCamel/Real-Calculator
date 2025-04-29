e2b5bcf047513d6fdd53c862e5f204e2f7f826ef
SattapriusCamel <oikkoandfriends90@gmail.com>
2025-04-29 20:41:32 +0200

    192

---
+++ java.js
@@ -0,0 +1,9 @@
+function getHistory() {
+    return document.getElementById("history_value").innerText;
+}
+function getOutput() {
+    return document.getElementById("output_value").innerText;
+}
+function PrintHistory(num) {
+    document.getElementById("history_value").innertext=num;
+}
